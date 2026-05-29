299f9abbcb1b899b4d02ec73f646fd881db23a38
ttnwwlss <shinsj02099@gmail.com>
2026-05-29 15:38:13 +0900

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,11 +1,3 @@
-// =========================================================
-// Supabase 연결 설정
-// =========================================================
-// Supabase Dashboard
-// → Project Settings
-// → API
-// → Project URL / anon public key 확인 후 아래에 붙여넣기
-
 const SUPABASE_URL = "https://zramxfjvcwibfzmujgzc.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpyYW14Zmp2Y3dpYmZ6bXVqZ3pjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwMTg5MjIsImV4cCI6MjA5NTU5NDkyMn0.uAy7pIrcirT9_lxRXEwjHZqu6i6bkimtP380nrRzgOU";
 
