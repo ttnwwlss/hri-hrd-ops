7ec2a187a139a653620b00c51ae46ca6d234b894
ttnwwlss <shinsj02099@gmail.com>
2026-05-29 17:21:08 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -407,7 +407,7 @@ function renderTimeline() {
   container.innerHTML = html;
 }
 
-function renderKanbanfunction renderKanban() {
+function renderKanban {
   const container = document.getElementById("kanbanView");
   const courses = getFilteredCourses();
 
